33757d3e1a41497240b2823e0c3296e35260b930
abigailprowse <79606542+abigailprowse@users.noreply.github.com>
2022-10-15 18:17:30 -0400

    start blue file template

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EarlySeasonSignalDropParkBlue.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "EarlySeasonSignalDropParkBlue", group="Iterative OpMode")
+public class EarlySeasonSignalDropParkBlue extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+        waitForStart();
+    }
+}
