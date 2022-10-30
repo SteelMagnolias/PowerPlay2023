fd8e7f64f4f4c917cb1383c7b02aedcfd1023bc2
abigailprowse <79606542+abigailprowse@users.noreply.github.com>
2022-10-29 20:58:44 -0400

    add auton pseudocode

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Earlyauton.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+public class Earlyauton {
+
+    //Add preloaded block
+
+    //If A5 or F2 cone not touch the touch sensor hehe it rythms
+
+    //If A2 or F5 make sure you push the button no duhhh
+
+    //If button is not pressed camera 1 on the left will activate just like poofff
+
+    //If button is pressed camera 2 on the right will activate yayyyyy
+
+    //If button is not pressed, the multiplier will be 1 which gives us default directions wahooy
+
+    //If button is pressed, the multiplier will be -1  which gives us opposite directions
+
+    //Camera reading
+
+    // ITS GO TIME LEVYYYYY aka start :)
+
+    //
+}
