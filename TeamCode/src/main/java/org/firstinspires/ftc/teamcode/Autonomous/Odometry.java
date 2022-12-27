fbd4aeecf76bca7835ad4d9198620a535226f66c
abigailprowse <79606542+abigailprowse@users.noreply.github.com>
2022-12-27 12:44:38 -0500

    added odometry test program.  will start the meat after I come back from lunch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Odometry.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+@Autonomous(name = "Odometry", group = "IterativeOpMode")
+
+public class Odometry extends LinearOpMode {
+
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    // wheels lol
+
+    @Override
+    public void runOpMode() {
+        // this is what happens in the autonomous code.
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+
+        waitForStart();
+    }
+}
