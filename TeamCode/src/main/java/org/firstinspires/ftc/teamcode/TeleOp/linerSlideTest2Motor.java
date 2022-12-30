33902d3ad9e7ebbe35b7190e841b97ab3a67ade4
veronica <vrluer@optonline.net>
2022-12-30 14:26:11 -0500

    my chrome book is stupid and wont connect to the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linerSlideTest2Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linerSlideTest2Motor.java
@@ -14,7 +14,7 @@ public class linerSlideTest2Motor extends OpMode {
     @Override
     public void init() {
         motor = hardwareMap.get(DcMotor.class, "motor");
-        motor = hardwareMap.get(DcMotor.class, "motor2");
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
     }
 
     @Override
@@ -24,14 +24,17 @@ public class linerSlideTest2Motor extends OpMode {
         if (leftY1 >= 0.1) {
             // up
             motor.setPower(0.5);
+            motor2.setPower(0.5);
         }
         else if (leftY1 <= 0.1) {
             // down
             motor.setPower(-0.5);
+            motor2.setPower(-0.5);
         }
         else {
             // off
             motor.setPower(0);
+            motor2.setPower(0);
         }
     }
 
